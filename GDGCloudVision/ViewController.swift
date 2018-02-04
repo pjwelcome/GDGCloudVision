2d9439bee793ff0e1ea128d7e3bc94537932b346
Peter-John Welcome <Peter-John.Welcome@Standardbank.co.za>
2018-02-04 22:21:52 +0200

    Initial Commit

---
+++ GDGCloudVision/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GDGCloudVision
+//
+//  Created by pjapple on 2018/02/04.
+//  Copyright © 2018 Multimeleon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
